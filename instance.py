7a7459b358de711809774d82ef8cde6a2604caed
FlavianoRS <83477815+FlavianoRS@users.noreply.github.com>
2021-05-04 10:26:44 -0300

    Add files via upload

---
+++ instance.py
@@ -0,0 +1,24 @@
+from flask import Flask, Blueprint
+from flask_restplus import Api
+
+
+class Server():
+    def __init__(self,):
+        self.app = Flask(__name__)
+        self.blueprint = Blueprint('api',__name__,url_prefix='/api')
+        self.api = Api(self.blueprint, doc='/docs', title='API Estágio')
+        self.app.register_blueprint(self.blueprint)
+
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data.db'
+        self.app.config['PROPAGATE_EXCEPTION'] = True
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        self.movie = self.movies()
+
+        def movies(self,):
+            return self.api.namespace(name='Movies', path='/')
+
+    def run(self, ):
+        self.app.run(port=5000, debug=True, host='0.0.0.0')
+
+server = Server()
